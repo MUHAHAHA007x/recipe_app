1e1093d524393a55492af989fbffaf7b33b9ebe1
MUHAHAHA007x <gamerkungv.1@gmail.com>
2025-12-02 09:58:17 +0700

    Working with Flutter Widgets

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/model/recipes';
+
+void main() {
+  runApp(const ReceipeApp());
+}
+
+class ReceipeApp extends StatelessWidget {
+  const ReceipeApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+      ),
+      home: const MyHomePage(title: 'Recipe Calculater'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: SafeArea(
+        child: Container(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return buildRecipeCard(Receipe.samples[index]); //Reuseฟังก์ชั่น
+            },
+            itemCount: Receipe.samples.length,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Receipe recipe) {
+    //ฟังก์ชั่นดีไซต์
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          Text(recipe.imgLable),
+        ],
+      ),
+    );
+  }
+}
